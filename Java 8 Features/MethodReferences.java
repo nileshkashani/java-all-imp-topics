0a27f5d916003b8c56ab83e83a6ee8ec4693f80a
nileshkashani <nileshkashani45@gmail.com>
2026-08-01 22:51:21 +0530

    update java 8 features

--- Java 8 Features/MethodReferences.java
+++ Java 8 Features/MethodReferences.java
@@ -1,18 +1,23 @@
+// Method references are a special type of lambda expressions. They’re often used to create simple lambda expressions by referencing existing methods.
+
+// There are four kinds of method references:
+
+//1. Static methods
+//2. Instance methods of particular objects
+//3. Instance methods of an arbitrary object of a particular type
+//4. Constructor
+
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
 
 public class MethodReferences {
     public static void main(String[] args) {
-        List<Integer> l = new ArrayList<>();
-        l.add(1000);
-        l.add(2);
-        l.add(3);
-        l.add(4);
-        l.stream()
-            .sorted()
-            .forEach(System.out::println);
-         
+        // 1. Static methods
+        List<String> messages = Arrays.asList("hello", "baeldung", "readers!");
+        messages.forEach(String::toUpperCase);  //forEach is a method of List interface which takes a Consumer functional interface as an argument.
+        
+        //2. Instance methods of particular objects
+        
     }
 }
