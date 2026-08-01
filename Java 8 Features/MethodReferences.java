a23cc52599e11f9823d2f65ff9f39fe8f2d7018a
nileshkashani <nileshkashani45@gmail.com>
2026-08-01 23:13:36 +0530

    updated java 8 features

--- Java 8 Features/MethodReferences.java
+++ Java 8 Features/MethodReferences.java
@@ -9,15 +9,30 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+
+class CompareInt implements Comparator<Integer>{
+    @Override
+    public int compare(Integer i1,Integer i2){
+        return i1.compareTo(i2);
+    }
+}
 
 public class MethodReferences {
     public static void main(String[] args) {
         // 1. Static methods
-        List<String> messages = Arrays.asList("hello", "baeldung", "readers!");
-        messages.forEach(String::toUpperCase);  //forEach is a method of List interface which takes a Consumer functional interface as an argument.
-        
+        List<Integer> messages = Arrays.asList(100, 50, 20);
+        messages.forEach(System.out::println);  //forEach is a method of List interface which takes a Consumer functional interface as an argument.\
+
         //2. Instance methods of particular objects
+        CompareInt c = new CompareInt();
+        messages.stream()
+            .sorted(c::compare)
+            .forEach(System.out::println);
+
+
+        //3. Instance methods of an arbitrary object of a particular type
         
     }
 }
